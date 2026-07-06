a271145e1bef67e04de6a64692cb7b28562c32c8
akularevanth3085-max <akularevanth3085@gmail.com>
2026-07-07 03:12:07 +0530

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 
 int main() {
-    // Seed the random number generator
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    double num1, num2;
+    char operation;
+    char continueChoice;
     
-    // Generate random number between 1 and 100
-    int secretNumber = std::rand() % 100 + 1;
-    int guess;
-    int attempts = 0;
-    
-    std::cout << "=== Number Guessing Game ===\n";
-    std::cout << "I have picked a number between 1 and 100.\n";
-    std::cout << "Can you guess it?\n\n";
+    std::cout << "=== Simple Calculator ===\n";
     
     do {
-        std::cout << "Enter your guess: ";
-        std::cin >> guess;
-        attempts++;
+        // Get input from user
+        std::cout << "\nEnter first number: ";
+        std::cin >> num1;
         
-        if (guess < secretNumber) {
-            std::cout << "Too low! Try again.\n";
-        } else if (guess > secretNumber) {
-            std::cout << "Too high! Try again.\n";
-        } else {
-            std::cout << "Congratulations! You guessed the number in " 
-                      << attempts << " attempts!\n";
+        std::cout << "Enter second number: ";
+        std::cin >> num2;
+        
+        std::cout << "Choose operation (+, -, *, /): ";
+        std::cin >> operation;
+        
+        // Perform calculation
+        switch (operation) {
+            case '+':
+                std::cout << num1 << " + " << num2 << " = " << (num1 + num2) << std::endl;
+                break;
+            case '-':
+                std::cout << num1 << " - " << num2 << " = " << (num1 - num2) << std::endl;
+                break;
+            case '*':
+                std::cout << num1 << " * " << num2 << " = " << (num1 * num2) << std::endl;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    std::cout << "Error: Division by zero is not allowed!\n";
+                } else {
+                    std::cout << num1 << " / " << num2 << " = " << (num1 / num2) << std::endl;
+                }
+                break;
+            default:
+                std::cout << "Error: Invalid operation! Please use +, -, *, or /\n";
         }
-    } while (guess != secretNumber);
+        
+        std::cout << "\nDo you want to perform another calculation? (y/n): ";
+        std::cin >> continueChoice;
+        
+    } while (continueChoice == 'y' || continueChoice == 'Y');
     
-    std::cout << "\nThanks for playing!\n";
+    std::cout << "\nThank you for using the calculator!\n";
     
     return 0;
 }
